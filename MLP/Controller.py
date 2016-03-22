bd964017fbe80932fda50eb1d65144263065c617
Mustafa Qamar-ud-Din <m.qamaruddin@nilecode.com>
2016-03-22 23:19:28 +0200

    normalize data

--- MLP/Controller.py
+++ MLP/Controller.py
@@ -1,5 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn import preprocessing
+import numpy
+
+from MLP import MLP
 
 class Controller:
     # dataset
@@ -13,6 +16,10 @@ class Controller:
     int_training_size = 30
     int_testing_size = 20
     int_num_per_class = 50
+    int_num_classes = 0
+
+    # composition
+    obj_mlp = ''
 
     # initializiation
     def __init__(self):
@@ -22,13 +29,18 @@ class Controller:
     # normalize or scale data
     # divide data to training and testing
     def prepareDataSet(self, iris):
+        self.int_num_classes = numpy.unique(iris.target).shape[0]
+
         self.int_set_size = iris.data.shape[1]
         self.int_num_features = iris.data.shape[0]
+
         ## normalize data
         self.data = preprocessing.normalize(iris.data)
+        self.data = preprocessing.minmax_scale(self.data, (-1, 1))
+        print self.data
 
         # load data in arrays
-        for i in range(0, self.int_set_size):
+        for i in range(0, len(self.data)):
             Y = iris.target[i]
             X = self.data[i]
             check_i = i % self.int_num_per_class
@@ -37,3 +49,10 @@ class Controller:
             else:
                 self.testing.append([X, Y])
 
+    # play the MLP
+    def playMLP(self):
+        # allow to test all combinations of settings
+        i = 1 ## number hidden layers
+        j = 1000 ## number of epochs
+        k = 20 ## number of hidden neurons
+        self.obj_mlp = MLP(self.int_num_features, self.int_num_classes, i, j, k)
--- MLP/MLP.py
+++ MLP/MLP.py
@@ -1,17 +1,35 @@
+import numpy
+
 class MLP:
     # properties
     int_num_epochs = 1000
     int_num_input_neurons = 0
     int_num_output_neurons = 0
     int_num_hidden_layers = 0
+    int_num_hidden_neurons = 0
+    dbl_mse_threshold = 0.001
 
     arr_num_neurons_in_hidden_layers = []
 
-    def __init__(self, _int_num_input_neurons, _int_num_output_neurons, _int_num_hidden_layers, _int_num_epochs):
+    # weights
+    wo = []
+    wh = []
+
+    ## initializations
+    def __init__(self, _int_num_input_neurons, _int_num_output_neurons, _int_num_hidden_layers, _int_num_epochs, _int_num_hidden_neurons):
+        ## initialize properties
         self.int_num_input_neurons = _int_num_input_neurons
         self.int_num_output_neurons = _int_num_output_neurons
         self.int_num_hidden_layers = _int_num_hidden_layers
         self.int_num_epochs = _int_num_epochs
+        self.int_num_hidden_neurons = _int_num_hidden_neurons
 
-        ## number of neurons in hidden layers ?
+        ## initialize weights arrays
+        self.wo = numpy.zeros(self.int_num_output_neurons, self.int_num_hidden_neurons + 1) ## bias +1
+        self.wh = numpy.zeros(self.int_num_hidden_neurons, self.int_num_input_neurons + 1) ## bias +1
 
+        return
+
+    ## back-propagation-algorithm
+    def train(self):
+        return
